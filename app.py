8f4512798ea081f92ae082165ca81404442e822c
G.Hemanth Sai <73033596+HemanthSai7@users.noreply.github.com>
2022-11-16 00:31:14 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-import dash
-from dash import html, dcc
+# import dash
+# from dash import html, dcc
 
-app = dash.Dash(__name__, use_pages=True)
+# app = dash.Dash(__name__, use_pages=True)
 
-app.layout = html.Div(
-    [
-        # main app framework
-        html.Div("Python Multipage App with Dash", style={'fontSize':50, 'textAlign':'center'}),
-        html.Div([
-            dcc.Link(page['name']+"  |  ", href=page['path'])
-            for page in dash.page_registry.values()
-        ]),
-        html.Hr(),
+# app.layout = html.Div(
+#     [
+#         # main app framework
+#         html.Div("Python Multipage App with Dash", style={'fontSize':50, 'textAlign':'center'}),
+#         html.Div([
+#             dcc.Link(page['name']+"  |  ", href=page['path'])
+#             for page in dash.page_registry.values()
+#         ]),
+#         html.Hr(),
 
-        # content of each page
-        dash.page_container
-    ]
-)
+#         # content of each page
+#         dash.page_container
+#     ]
+# )
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+from referenceScripts.dashboard import app
