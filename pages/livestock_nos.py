a31c9d30dd171e77125cf370f8929790f885c9ec
Sayan Saha <sahensha1600@gmail.com>
2022-11-15 11:28:15 +0530

    dashboard

---
+++ pages/livestock_nos.py
@@ -0,0 +1,14 @@
+import dash
+from dash import dcc, html
+
+dash.register_page(__name__)
+
+layout = html.Div(children=[
+    html.H1(children='This is our Home page'),
+
+    html.Div(children='''
+        This is our Home page content.
+    '''),
+
+])
+
