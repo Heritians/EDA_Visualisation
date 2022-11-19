a1e35110542a28956c89c8913be2472811c4a91b
G.Hemanth Sai <73033596+HemanthSai7@users.noreply.github.com>
2022-11-19 16:54:05 +0530

    fixed plotting issue

--- dashboard/vizfuncs/table1to6.py
+++ dashboard/vizfuncs/table1to6.py
@@ -2,34 +2,35 @@ from dash import html,dcc
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
+import numpy as np
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 
 from app import app
 from ..utils.parseresponsejson import DATA
 
+
 layout=html.Div([
-    dbc.Container([
-        dbc.Row([
-            dbc.Col([
-                dcc.Graph(id="agri_prod")
+    dbc.Row([
+        dbc.Col([
+            dcc.Graph(id="agri_prod")
             ])
         ])
     ])
-])
 
 # print(DATA["Sehore"]['agri_products']['crop_name'].values)
-@app.callback([Output("agri_prod","figure")],
+@app.callback(Output("agri_prod","figure"),
               [Input('village_name', 'value')])
-def agri_products(village_name="Sehore"):
-    print(village_name)
+def agri_products(village_name):
+    data=np.unique(DATA[village_name]['agri_products']['crop_name'].values)
+    data=dict.fromkeys(data,0)
+    for i in range(len(DATA[village_name]['agri_products']['crop_name'].values)):
+        data[DATA[village_name]['agri_products']['crop_name'].values[i]]+=DATA[village_name]['agri_products']['crop_area_prev_yr_acre'].values[i]    
     fig=go.Figure(data=[
-        go.Bar(name='Crops', x=DATA[village_name]['agri_products']['crop_name'].values, y=DATA[village_name]['agri_products']['crop_area_prev_yr_acre'].values),
+        go.Bar(name='Crops', x=list(data.keys()), y=list(data.values())),
     ])
-    
-    # fig=plt.bar(x=DATA[village_name]['agri_products']['crop_name'].values, height=DATA[village_name]['agri_products']['crop_area_prev_yr_acre'].values)
     fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',
                       plot_bgcolor='rgba(0,0,0,0)',
                       template = "seaborn",
                       margin=dict(t=0))
-    return dcc.Graph(figure=fig)                 
+    return fig               
--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-import dash
-from dash import html, dcc
+# import dash
+# from dash import html, dcc
 
-app = dash.Dash(__name__)
+# app = dash.Dash(__name__)
 
-app.layout = html.Div(
-    [
-        # main app framework
-        html.Div("Connecting Villages", style={'fontSize':50, 'textAlign':'center'}),
-        #dcc.Dropdown(['Kothri', 'Ashta', 'Sehore','Ujjain','Indore'], 'Indore', id='demo-dropdown'),
+# app.layout = html.Div(
+#     [
+#         # main app framework
+#         html.Div("Connecting Villages", style={'fontSize':50, 'textAlign':'center'}),
+#         #dcc.Dropdown(['Kothri', 'Ashta', 'Sehore','Ujjain','Indore'], 'Indore', id='demo-dropdown'),
         
-        html.Div([
-            "Village Names",
-             dcc.Dropdown(['Kothri', 'Ashta', 'Sehore','Ujjain','Indore'], 'Indore', id='demo-dropdown')
-        ]),
-        html.Hr(),
+#         html.Div([
+#             "Village Names",
+#              dcc.Dropdown(['Kothri', 'Ashta', 'Sehore','Ujjain','Indore'], 'Indore', id='demo-dropdown')
+#         ]),
+#         html.Hr(),
 
-        # content of each page
-        dash.page_container
-    ]
-)
+#         # content of each page
+#         dash.page_container
+#     ]
+# )
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
