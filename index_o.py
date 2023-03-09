7c02671c4b107ca839adb802f069ff8b480f9cd3
Sayan Saha <sahensha1600@gmail.com>
2023-03-10 00:42:11 +0530

    add css

--- index_o.py
+++ index_o.py
@@ -7,32 +7,21 @@ import pandas as pd
 from app import server
 from app import app
 
-from dashboard.vizfuncs import table1to6
+from dashboard.vizfuncs import table1to6_old
 from dashboard.utils.parseresponsejson import VILLAGE_NAMES,get_data, DATA
+import sys
+print(sys.executable)
 
 dropdown=dcc.Dropdown(
         id='village_name',
         options=[{"label":i,"value":i} for i in VILLAGE_NAMES],
-        value="Lasudiya_Khas",
+        value="Sehore",
         multi=False,
-        style={'width': '70%', 'margin-left': '1px', }
+        style={'width': '70%', 'margin-left': '5px'}
     )
 
 app.layout = html.Div([
-    html.Div([
-    dbc.Row([
-    dbc.Col([dbc.CardImg(src = 'assets\logo_sm.png', top = True),
-    ], width=1),
-    dbc.Col([
-    html.Br(),
-    html.Br(),
-    html.H1('Heritians - EDA Dashboard')
-    ]),
-    ]),
     dropdown,
-    ], className='header'),
-    html.Br(),
-    html.Br(),
     html.Div(id='page-content')
 ])
 
@@ -44,7 +33,7 @@ def display_page(pathname):
     DATA.update({pathname:data})
     df = pd.DataFrame(DATA[pathname]['fam_info'])
     df.to_csv('datasets/family.csv',index=False)
-    return table1to6.layout    
+    return table1to6_old.layout    
 
 if __name__ == '__main__':
     app.run_server(debug=True,use_reloader=True)
